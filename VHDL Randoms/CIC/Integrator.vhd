266027556d1a7f02bf422997942fdc6724861aed
Aosola <jhisme@gmail.com>
2015-01-26 19:35:03 -0500

    Update Integrator.vhd

--- VHDL Randoms/CIC/Integrator.vhd
+++ VHDL Randoms/CIC/Integrator.vhd
@@ -6,14 +6,14 @@ use IEEE.MATH_REAL.ALL;
 ENTITY Integrator IS
     GENERIC(
         D_WIDTH_in  :POSITIVE := 2;
-        Truncation  :NATURAL  := 0;
+        Truncation  :NATURAL  := 0
     );
     PORT(
         RESET   :in  STD_LOGIC;
         CLK     :in  STD_LOGIC;
         
         X       :in  SIGNED(D_WIDTH-1 downto 0);
-        Y       :out SIGNED(D_WIDTH_in-Truncation-1 downto 0);
+        Y       :out SIGNED(D_WIDTH_in-Truncation-1 downto 0)
     );
 END ENTITY Integrator;
 
